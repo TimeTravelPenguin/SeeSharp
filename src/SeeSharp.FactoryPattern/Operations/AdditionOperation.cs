f5dbfef3d9990034ce79f1d3662f2d695fd4ced9
Phillip <FilupErwinSmith@gmail.com>
2021-12-18 12:37:22 +1100

    Added header

--- src/SeeSharp.FactoryPattern/Operations/AdditionOperation.cs
+++ src/SeeSharp.FactoryPattern/Operations/AdditionOperation.cs
@@ -1,4 +1,20 @@
-﻿namespace SeeSharp.FactoryPattern.Operations;
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: SeeSharp.
+// Project: SeeSharp.Shared
+// File Name: AdditionOperation.cs
+// 
+// Current Data:
+// 2021-12-18 12:00 PM
+// 
+// Creation Date:
+// 2021-12-18 11:59 AM
+
+#endregion
+
+namespace SeeSharp.FactoryPattern.Operations;
 
 internal class AdditionOperation : IBinaryOperation
 {
--- src/SeeSharp.FactoryPattern/Operations/SubtractionOperation.cs
+++ src/SeeSharp.FactoryPattern/Operations/SubtractionOperation.cs
@@ -1,4 +1,20 @@
-﻿namespace SeeSharp.FactoryPattern.Operations;
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: SeeSharp
+// Project: SeeSharp.FactoryPattern
+// File Name: SubtractionOperation.cs
+// 
+// Current Data:
+// 2021-12-18 12:36 PM
+// 
+// Creation Date:
+// 2021-12-18 12:34 PM
+
+#endregion
+
+namespace SeeSharp.FactoryPattern.Operations;
 
 internal class SubtractionOperation : IBinaryOperation
 {
